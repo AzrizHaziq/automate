ccde2a13ee02a289496d3b765f609049a4774d90
azrizhaziq <azrizhaziq@gmail.com>
2021-06-25 16:00:45 +0800

    test tsi

---
+++ abc.ts
@@ -0,0 +1,72 @@
+import fs from 'fs'
+import { chromium } from 'playwright-chromium'
+
+async function scrapBursaMalaysia() {
+  const scrapUrl = ({ per_page, page }) =>
+    `https://www.bursamalaysia.com/market_information/equities_prices?legend[]=[S]&sort_by=short_name&sort_dir=asc&page=${page}&per_page=${per_page}`
+
+  try {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    await page.goto(scrapUrl({ page: 1, per_page: 50 }))
+
+    // getting max size of syariah list by grabbing the value in pagination btn
+    const maxPageNumbers = await page.evaluate(() => {
+      const paginationBtn = Array.from(document.querySelectorAll('.pagination li [data-val]'))
+        .map(i => i.textContent)
+        .filter(Boolean)
+        .map(parseFloat)
+
+      return Math.max(...paginationBtn)
+    })
+
+    let syariahList = {}
+
+    // grab all syariah list and navigate to each pages.
+    for (let i = 1; i <= maxPageNumbers; i++) {
+      await page.goto(scrapUrl({ page: i, per_page: 50 }), {
+        waitUntil: 'networkidle',
+      })
+
+      const temp = await page.evaluate(() => {
+        const pipe =
+          (...fn) =>
+          initialVal =>
+            fn.reduce((acc, fn) => fn(acc), initialVal)
+        const removeSpaces = pipe(name => name.replace(/\s/gm, ''))
+        const removeSpacesAndShariah = pipe(removeSpaces, name => name.replace(/\[S\]/gim, ''))
+
+        return Array.from(document.querySelectorAll('.dataTables_scrollBody table tbody tr')).reduce((acc, tr) => {
+          const s = tr.querySelector(':nth-child(2)').textContent
+          const stockCode = tr.querySelector(':nth-child(3)').textContent
+
+          const code = removeSpaces(stockCode)
+          const stockName = removeSpacesAndShariah(s)
+          return {
+            ...acc,
+            [code]: {
+              s: 1,
+              code,
+              stockName,
+            },
+          }
+        }, {})
+      })
+
+      syariahList = { ...syariahList, ...temp }
+    }
+
+    await browser.close()
+
+    console.log('\n\nFound: ', Object.keys(syariahList).length)
+
+    return syariahList
+  } catch (e) {
+    console.error('Error scrap data', e)
+    process.exit(1)
+  }
+}
+
+scrapBursaMalaysia().then(j => {
+  fs.writeFileSync('abc.json', JSON.stringify(j, null, 2))
+})
