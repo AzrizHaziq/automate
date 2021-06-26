7ffb3b4a90fcf27353736be27e7649b2ea40c7ee
azrizhaziq <azrizhaziq@gmail.com>
2021-06-26 20:37:20 +0800

    removing few cols in table for confidential
    - MITI_PASS and ICS are required in env

--- tests/bsn.spec.ts
+++ tests/bsn.spec.ts
@@ -3,25 +3,26 @@ import { test, expect } from '@playwright/test'
 const ics: string[] = (process.env.ICS || '').split(', ')
 
 test.describe('Check BSN SSP with my ic', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.setExtraHTTPHeaders({ 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8' })
-    await page.goto('https://www.bsn.com.my/page/bsn-ssp-draw-results?lang=ms-MY')
-    await page.$('#ssp-search').then(i => i.scrollIntoViewIfNeeded())
+  test.beforeEach(async ({ page: p }) => {
+    test.skip()
+
+    await p.setExtraHTTPHeaders({ 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8' })
+    await p.goto('https://www.bsn.com.my/page/bsn-ssp-draw-results?lang=ms-MY')
+    await p.$('#ssp-search').then(i => i.scrollIntoViewIfNeeded())
   })
 
   ics.forEach(ic => {
-    test(`Test with`, async ({ page }) => {
-      await page.screenshot({ path: 'load.png' })
-      await page.waitForSelector('#ssp-search')
-      await page.fill('#ssp-search .bsn-custom-input', ic)
-      await page.evaluate(body => body.focus(), await page.$('body'))
+    test(`Test with`, async ({ page: p }) => {
+      await p.screenshot({ path: 'load.png' })
+      await p.waitForSelector('#ssp-search')
+      await p.fill('#ssp-search .bsn-custom-input', ic)
+      await p.evaluate(body => body.focus(), await p.$('body'))
 
-      await page.click('#ssp-search button', { force: true })
-      await page.dispatchEvent('#ssp-search button', 'click')
+      await p.click('#ssp-search button', { force: true })
+      // await p.dispatchEvent('#ssp-search button', 'click')
 
-      await page.waitForTimeout(2000)
-      await page.waitForSelector('.ssp-winner-inner h3')
-      const result = await page.$eval('.ssp-winner-inner h3', i => i.textContent)
+      await p.waitForSelector('.ssp-winner-inner h3')
+      const result = await p.$eval('.ssp-winner-inner h3', i => i.textContent)
 
       expect(result).toBe('Tiada hasil padanan.')
     })
--- tests/miti.spec.ts
+++ tests/miti.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test'
 
+const ic = (process.env.ICS || '').split(', ')[0]
+const pass = process.env.MITI_PASS
+
 test.describe('MITI', () => {
   test.beforeEach(async ({ page: p }) => {
     await p.setExtraHTTPHeaders({ 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8' })
@@ -15,9 +18,25 @@ test.describe('MITI', () => {
     await p.fill('input.login', ic)
     await p.fill('input.password', pass)
     await p.click('#go')
-
     await p.click('text=My Saham')
 
-    await p.waitForTimeout(5000)
+    const el = await p.$('table')
+
+    await p.evaluate(() => {
+      const leaveColsTo = 3
+      const tableEl = document.querySelector('table')
+
+      Array.from(tableEl.rows).forEach(row => {
+        const maxCols = row.children.length
+
+        for (let i = 0; i <= maxCols; i++) {
+          if (row.cells[leaveColsTo]) {
+            row.cells[leaveColsTo].remove()
+          }
+        }
+      })
+    })
+
+    expect(await el.screenshot()).toMatchSnapshot('status_miti.png')
   })
 })
